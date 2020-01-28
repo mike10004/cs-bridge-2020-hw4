426aa3e75e4896b09e8f33b087b16b69c9dfd9ed
Mike Chaberski <mchaberski@gmail.com>
2020-01-27 23:41:52 -0500

    make prepare.sh hardier

--- prepare.sh
+++ prepare.sh
@@ -9,13 +9,22 @@ PREFIX_FILE="$OUTPUT_DIR/.prefix"
 if [ -z "$PREFIX" ] ; then
   if [ -f "$PREFIX_FILE" ] ; then
     PREFIX=$(cat "$PREFIX_FILE")
+    echo "prefix $PREFIX read from $PREFIX_FILE"
   else
+    echo "no stored prefix found in $PREFIX_FILE" >&2
     echo "must specify prefix (e.g. 'net123_hw4_') as first argument" >&2
     exit 1
   fi
+else
+  mkdir -p "$OUTPUT_DIR"
+  echo "$PREFIX" > "$PREFIX_FILE"
+  echo "prefix $PREFIX stored in $PREFIX_FILE" >&2
 fi
 
-set -e
+if [ -z "$PREFIX" ] ; then
+  echo "prefix not valid; fix $PREFIX_FILE or specify argument if you try again" >&2
+  exit 1
+fi 
 
 for QDIR in ./q*
 do
@@ -24,6 +33,6 @@ do
   QBASE="$(basename "$QDIR")"
   DST="${OUTPUT_DIR}/${PREFIX}${QBASE}.cpp"
   mkdir -p "$(dirname "$DST")"
-  cp -nv "$CPP_FILE" "$DST"
+  cp -v "$CPP_FILE" "$DST"
 done
 
