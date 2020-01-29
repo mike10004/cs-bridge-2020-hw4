7a7da2077c1d5819158bcb257d3067c12a8bf40b
Mike Chaberski <mchaberski@gmail.com>
2020-01-28 23:44:01 -0500

    build: support env var specification of cmake/make location

--- build.sh
+++ build.sh
@@ -2,13 +2,19 @@
 
 set -e
 
+CMAKE="${CMAKE:-cmake}"
+MAKE="${MAKE:-make}"
+
+command -V "$CMAKE"
+command -V "$MAKE"
+
 THIS_DIR=$(dirname "$0")
 BUILD_DIR="${THIS_DIR}/cmake-build-debug"
 
-cmake -DCMAKE_BUILD_TYPE=Debug -S "${THIS_DIR}" -B "${BUILD_DIR}"
+"$CMAKE" -DCMAKE_BUILD_TYPE=Debug -S "${THIS_DIR}" -B "${BUILD_DIR}"
 
 pushd "${BUILD_DIR}" >/dev/null
 
-make
+"$MAKE"
 
 popd >/dev/null
