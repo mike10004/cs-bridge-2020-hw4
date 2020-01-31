6a08bc525fbfcaed4ae0a3493e42bd02ba5f3c1e
Mike Chaberski <mchaberski@novetta.com>
2020-01-31 18:35:16 -0500

    q1 implemented with test cases

--- q1/main.cpp
+++ q1/main.cpp
@@ -1,11 +1,31 @@
+// mac937@nyu.edu
+// Tandon CS Bridge Winter 2020
+// Week 4
+// Question 1
+
 #include <iostream>
-#include <string>
+
+using namespace std;
 
 int main()
 {
-    std::string name;
-    std::cout << "Please enter your name: ";
-    std::cin >> name;
-    std::cout << "hello, " << name << std::endl;
+    int numEvenNumbersToPrint;
+    cout << "Please enter a positive integer: ";
+    cin >> numEvenNumbersToPrint;
+
+    cout << "section a" << endl;
+    int printedNumberCount = 0;
+    while (printedNumberCount < numEvenNumbersToPrint)
+    {
+        cout << (2 * (printedNumberCount + 1)) << endl;
+        printedNumberCount++;
+    }
+
+    cout << "section b" << endl;
+    for (int i = 0; i < numEvenNumbersToPrint; i++)
+    {
+        cout << (2 * (i + 1)) << endl;
+    }
+
     return 0;
 }
--- q1/question.md
+++ q1/question.md
@@ -1,3 +1,14 @@
 # Question 1
 
-Write a program...
+Write two versions of a program that reads a positive integer *n*, and prints the first *n* even
+numbers.
+
+a) In the first, use a while loop.
+b) In the second, use a for loop.
+
+Each section should interact with the user exactly as it shows in the following example:
+
+    Please enter a positive integer: 3
+    2
+    4
+    6
