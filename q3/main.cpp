9b177360be1390394e6cc0149932b88293959c35
Mike Chaberski <mchaberski@gmail.com>
2020-01-31 21:42:13 -0500

    q3: finish

--- q3/main.cpp
+++ q3/main.cpp
@@ -1,4 +1,5 @@
 // Question 3
+// Behavior for negative integers is undefined
 
 #include <iostream>
 
@@ -6,6 +7,30 @@ using namespace std;
 
 int main()
 {
-    cout << "q3 executed" << endl;
+    int decimal;
+    cout << "Enter decimal number:\n";
+    cin >> decimal;
+
+    int numBinaryDigits = 0;
+    int intPart = decimal;
+    do {
+        intPart /= 2;
+        numBinaryDigits++;
+    } while (intPart > 0);
+
+    cout << "The binary representation of " << decimal << " is ";
+    for (int exponent = numBinaryDigits - 1; exponent >= 0; exponent--) {
+        int powerOf2 = 1;
+        for (int i = 0; i < exponent; i++) {
+            powerOf2 *= 2;
+        }
+        if (decimal >= powerOf2) {
+            cout << '1';
+            decimal -= powerOf2;
+        } else {
+            cout << '0';
+        }
+    }
+    cout << endl;
     return 0;
 }
