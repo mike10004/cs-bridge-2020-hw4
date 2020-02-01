57bcd85ba783b37b578db5f0c2e06b4e1cc4b7dd
Mike Chaberski <mchaberski@gmail.com>
2020-01-31 23:05:14 -0500

    q5: impl

--- q5/main.cpp
+++ q5/main.cpp
@@ -6,6 +6,26 @@ using namespace std;
 
 int main()
 {
-    cout << "q5 executed" << endl;
+    int hourglassHalfHeight;
+    cout << "Enter a positive integer: ";
+    cin >> hourglassHalfHeight;
+    for (int row = hourglassHalfHeight - 1; row >= 0; row--) {
+        for (int col = 0; col < (hourglassHalfHeight - row - 1); col++) {
+            cout << ' ';
+        }
+        for (int star = 0; star < (row * 2 + 1); star++) {
+            cout << '*';
+        }
+        cout << endl;
+    }
+    for (int row = 0; row < hourglassHalfHeight; row++) {
+        for (int col = 0; col < (hourglassHalfHeight - row - 1); col++) {
+            cout << ' ';
+        }
+        for (int star = 0; star < (row * 2 + 1); star++) {
+            cout << '*';
+        }
+        cout << endl;
+    }
     return 0;
 }
