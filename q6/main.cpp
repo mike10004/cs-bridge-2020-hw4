ea0f1e9ffbcb7dcb3c4e8d19236d984e7e969870
Mike Chaberski <mchaberski@gmail.com>
2020-01-31 23:38:27 -0500

    q6: implement with tests

--- q6/main.cpp
+++ q6/main.cpp
@@ -1,11 +1,32 @@
 // Question 6
+// Note: behavior is undefined for non-positive integers
 
 #include <iostream>
 
 using namespace std;
 
+int BASE = 10;
+
 int main()
 {
-    cout << "q6 executed" << endl;
+    int positiveInt;
+    cout << "Enter a positive integer: ";   
+    cin >> positiveInt;
+    for (int i = 0; i <= positiveInt; i++) {
+        int numDigits = 0, numEvenDigits = 0, numOddDigits;
+        int integerPart = i;
+        while (integerPart > 0) {
+            int remainder = integerPart % BASE;
+            integerPart /= BASE;
+            if ((remainder % 2) == 0) {
+                numEvenDigits++;
+            }
+            numDigits++;
+        }
+        numOddDigits = numDigits - numEvenDigits;
+        if (numEvenDigits > numOddDigits) {
+            cout << i << endl;
+        }
+    }
     return 0;
 }
