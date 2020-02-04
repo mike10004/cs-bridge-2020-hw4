c3aee7ee006a3fbcd6c275868491d9a809710bce
Mike Chaberski <mchaberski@gmail.com>
2020-02-03 20:34:32 -0500

    q4: fix typo in prompt

--- q4/main.cpp
+++ q4/main.cpp
@@ -4,6 +4,10 @@
 // Question 4
 // Program that prints the geometric mean of a sequence of positive integers
 
+// Note: behavior is undefined if the input sequence contains negative 
+//       integers (in section (a)) or the product of the input integers 
+//       is greater than what an int can store (in either section)
+
 #include <iostream>
 #include <cmath>
 
@@ -11,31 +15,42 @@ using namespace std;
 
 int main()
 {
-    int sequenceLength, sequenceElement;
-    int product = 1;
-    cout << "section a\n\n";
-    cout << "Please enter the length of the sequence: ";
-    cin >> sequenceLength;
-    cout << "Please enter your sequence:\n";
-    for (int i = 0; i < sequenceLength; i++) {
-        cin >> sequenceElement;
-        product *= sequenceElement;
-    }
-    double geometricMean = pow(product, 1.0 / sequenceLength);
-    cout << "The geometric mean is: " << geometricMean << endl;
-    cout << "\nsection b\n\n";
-    product = 1;
-    sequenceLength = 0;
-    cout << "Please enter a non-empty sequence of positive integers, each one in a \n"
-            "separate line. End your sequence by typing -1:\n";
-    do { 
-        cin >> sequenceElement;
-        if (sequenceElement > 0) {
+    // Section (a)
+    {
+        int sequenceLength, sequenceElement;
+        int product = 1;
+        cout << "section a\n\n";
+        cout << "Please enter the length of the sequence: ";
+        cin >> sequenceLength;
+        cout << "Please enter your sequence:\n";
+        for (int i = 0; i < sequenceLength; i++) {
+            cin >> sequenceElement;
             product *= sequenceElement;
-            sequenceLength++;
         }
-    } while (sequenceElement > 0);
-    geometricMean = pow(product, 1.0 / sequenceLength);
-    cout << "The geometric mean is: " << geometricMean << endl;
+        
+        double geometricMean = pow(product, 1.0 / sequenceLength);
+        cout << "The geometric mean is: " << geometricMean << endl;
+    }
+
+    cout << endl;
+
+    // Section (b)
+    {
+        int sequenceLength = 0, sequenceElement;
+        int product = 1;
+        cout << "section b\n\n";
+        cout << "Please enter a non-empty sequence of positive integers, each one on a \n"
+                "separate line. End your sequence by typing -1:\n";
+        do {
+            cin >> sequenceElement;
+            if (sequenceElement > 0) {
+                product *= sequenceElement;
+                sequenceLength++;
+            }
+        } while (sequenceElement > 0);
+
+        double geometricMean = pow(product, 1.0 / sequenceLength);
+        cout << "The geometric mean is: " << geometricMean << endl;
+    }
     return 0;
 }
